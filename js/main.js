df470c0f91b1ad2365dcb224dc43d04425ea17fb
omar rodriguez <omar.rodriguez2108@hotmail.com>
2022-05-18 14:41:11 -0500

    corrigiendo archivo JavaScript

--- js/main.js
+++ js/main.js
@@ -2,7 +2,25 @@
 const cambiar = () => {
     
     let cambio = document.getElementById("cambio").value;
-    document.body.style.backgroundColor = cambio;
+
+    switch (cambio) {
+        case "1":
+            document.body.style.backgroundColor ="#f4f6f5";
+            break;
+        case "2":
+            document.body.style.backgroundColor ="#40df82";
+            break;
+        case "3":
+            document.body.style.backgroundColor ="#087bcd";
+            break; 
+        case "4":
+            document.body.style.backgroundColor ="#cd0808";
+            break; 
+        default:
+            document.body.style.backgroundColor ="#FFFFFF";
+            break;
+    }
+    
 
 }
 
